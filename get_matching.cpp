bf03e937a885ee8bb02ba541f681c2fad98ed8d7
Kirill Bessonov <mr.bk43@yandex.ru>
2021-09-18 21:23:33 +0300

    add func galesheply

---
+++ get_matching.cpp
@@ -0,0 +1,2 @@
+
+std::unique_ptr<Matching> GaleShapleyByGender(std::unique_ptr<Graph> graph, Gender gender);
--- graph_generation.cpp
+++ graph_generation.cpp
@@ -1,3 +1,3 @@
 std::unique_ptr<Graph> GraphGenerating(int n, int k, ld beta, int alpha) {
-
+    
 }
